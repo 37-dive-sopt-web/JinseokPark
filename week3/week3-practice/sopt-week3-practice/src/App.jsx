6b65368311b5020db4aeef1f5b5cdb991ff57b57
JS MacBook Pro <j_seokpark@naver.com>
2025-11-01 17:47:16 +0900

    3주차 추가 실습 구현

--- week3/week3-practice/sopt-week3-practice/src/App.jsx
+++ week3/week3-practice/sopt-week3-practice/src/App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import data from "./member.js";
 import Card from "./components/Card.jsx";
 import Header from "./components/Header.jsx";
+import Search from "./components/Search.jsx";
+import useSearch from "./hooks/useSearch.js";
 
 function App() {
+  const { search, filteredMembers, handleSearchChange, handleSearch } =
+    useSearch(data);
+
   return (
     <>
       <Header />
+      <Search
+        search={search}
+        handleSearchChange={handleSearchChange}
+        handleSearch={handleSearch}
+      />
       <div className="card_wrapper">
-        {data.map((memberData) => (
+        {filteredMembers.map((memberData) => (
           <Card
             key={memberData.id}
             name={memberData.name}
