2f863bc62d6408f967b49602f99bac27a6e1ac74
JS MacBook Pro <j_seokpark@naver.com>
2025-11-16 17:45:49 +0900

    Feat: 화면 크기 축소 시 메뉴바 구현

--- week4/week4-assignment/src/pages/MyPage/PageLayout/MyPageLayout.tsx
+++ week4/week4-assignment/src/pages/MyPage/PageLayout/MyPageLayout.tsx
@@ -1,4 +1,9 @@
-import { myPageStyle, headerStyle } from "./MyPageLayout.css";
+import {
+  myPageStyle,
+  headerStyle,
+  menuBtn,
+  navMenuContainer,
+} from "./MyPageLayout.css";
 import { useUserInfo } from "../../../hooks/useUserInfo";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -9,9 +14,14 @@ const PageLayout = () => {
   const navigate = useNavigate();
   const displayName = userName;
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleNav = (path: string) => {
     navigate(path);
+  };
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
   };
 
   return (
@@ -21,12 +31,17 @@ const PageLayout = () => {
           <h2>마이페이지</h2>
           <p>안녕하세요, {displayName}님</p>
         </div>
-        <nav>
-          <p onClick={() => handleNav("/mypage")}>내 정보</p>
-          <p onClick={() => handleNav("/mypage/members")}>회원 조회</p>
-          <p onClick={() => handleNav("/")}>로그아웃</p>
-          <p onClick={() => setIsModalOpen(true)}>회원 탈퇴</p>
-        </nav>
+        <div>
+          <button className={menuBtn} onClick={handleToggleMenu}>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+          <nav className={`${navMenuContainer} ${isMenuOpen ? "open" : ""}`}>
+            <p onClick={() => handleNav("/mypage")}>내 정보</p>
+            <p onClick={() => handleNav("/mypage/members")}>회원 조회</p>
+            <p onClick={() => handleNav("/")}>로그아웃</p>
+            <p onClick={() => setIsModalOpen(true)}>회원 탈퇴</p>
+          </nav>
+        </div>
       </header>
       <main>
         <Outlet />
